d6d7768cc4bb0b83e85df12eed7bbee2927564b2
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-07-26 20:16:56 +0530

    [Kanak] Adds the signup and signin component and add the routes in the app.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './component/layout/Navbar'
 import Dashboard from './component/dashboard/Dashboard'
 import ProjectDetails from './component/projects/ProjectDetails'
+import SignIn from './component/auth/SignIn';
+import SignUp from './component/auth/SignUp';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Switch>
       <Route exact path='/' component={Dashboard} />
       <Route path='/project/:id' component={ProjectDetails} />
+      <Route path='/signin' component={SignIn} />
+      <Route path='/signout' component={SignUp} />
     </Switch>
     </div>
     </BrowserRouter>
