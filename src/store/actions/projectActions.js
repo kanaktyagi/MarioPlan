f65789ad60dcfc910eb6fd4a960b80eca3441ff6
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-10 11:48:15 +0530

    Added the created project to the firestore using thunk middleware

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,9 +1,17 @@
 export const createProject = (project) => {
-    return(dispatch,getState) => {
+    return(dispatch,getState, {getFirebase, getFirestore}) => {
 
-        dispatch({
-            type:'CREATE_PROJECT',
-            project
-        });
+        const firestore = getFirebase().firestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Net',
+            authorLastName: 'Ninja',
+            authorId: 12345,
+            createAt: new Date()
+        }).then(() => {
+            dispatch({ type:'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERR', err});
+        })
     }
 }
