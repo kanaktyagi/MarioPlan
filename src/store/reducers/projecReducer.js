f1d81676177046556349985dab07ad5c5970a5da
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-10 11:49:29 +0530

    Logged the data to the console in the reducer

--- src/store/reducers/projecReducer.js
+++ src/store/reducers/projecReducer.js
@@ -8,11 +8,15 @@ const initState = {
 
 const projectReducer = (state = initState, action) => {
     switch(action.type) {
-        case 'CREATE_PROJECT' :
-            console.log(action.project)
-        
+        case 'CREATE_PROJECT':
+            console.log(action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.err);
+            return state;
+        default: 
+        return state;        
     }
-    return state
 }
 
 export default projectReducer
